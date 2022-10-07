a04b51686935869d6c60d86a7b1f03719ac3d97d
grantbuster <grant.buster@nrel.gov>
2022-10-07 12:27:02 -0600

    added custom Sup3rConcat layer

--- phygnn/layers/custom_layers.py
+++ phygnn/layers/custom_layers.py
@@ -588,3 +588,28 @@ class Sup3rAdder(tf.keras.layers.Layer):
             Output tensor with the hi_res_adder added to x.
         """
         return x + hi_res_adder
+
+
+class Sup3rConcat(tf.keras.layers.Layer):
+    """Layer to concatenate a high-resolution feature to a sup3r model in the
+    middle of a super resolution forward pass."""
+
+    def call(self, x, hi_res_feature):
+        """concatenates a hi-resolution feature to the input tensor x in the
+        middle of a sup3r resolution network.
+
+        Parameters
+        ----------
+        x : tf.Tensor
+            Input tensor
+        hi_res_feature : tf.Tensor | np.ndarray
+            This should be a 4D array for spatial enhancement model or 5D array
+            for a spatiotemporal enhancement model (obs, spatial_1, spatial_2,
+            (temporal), features) that can be concatenated to x.
+
+        Returns
+        -------
+        x : tf.Tensor
+            Output tensor with the hi_res_feature added to x.
+        """
+        return tf.concat((x, hi_res_feature), axis=-1)
