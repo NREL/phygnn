07b576ed7a3892f10afa9a4359ee69bd305eae95
grantbuster <grant.buster@nrel.gov>
2023-11-10 10:50:16 -0700

    tensorflow currently does not support py3.12

--- setup.py
+++ setup.py
@@ -64,7 +64,6 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
     ],
     test_suite="tests",
     python_requires='>=3.8',
